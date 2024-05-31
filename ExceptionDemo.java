d7cc92fed7ac35492e5dd6dbad1607e91b451341
adityajani11 <67045611+adityajani11@users.noreply.github.com>
2024-05-31 17:15:20 +0530

    Update ExceptionDemo.java

--- ExceptionDemo.java
+++ ExceptionDemo.java
@@ -13,6 +13,9 @@ public class ExceptionDemo {
         } catch (NullPointerException ne) {
             System.out.println("NullPointerException Occurred");
         } finally {
+            // Java finally block is a block used to execute important code such as closing the connection, etc.
+            // Java finally block is always executed whether an exception is handled or not.
+            // Therefore, it contains all the necessary statements that need to be printed regardless of the exception occurs or not.
             System.out.println("Finally Block Executed");
         }
     }    
